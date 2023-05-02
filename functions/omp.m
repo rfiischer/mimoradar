7881e892047b94196e091611103011cad0f907e2
Rodrigo Cardoso Rodrigues Fischer <ic62iciz@cip115-26.e-technik.uni-erlangen.de>
2023-05-02 17:02:15 +0200

    Check for Conditioning in OMP

--- functions/omp.m
+++ functions/omp.m
@@ -1,8 +1,9 @@
-function [xHat, S] = omp(y, A, tol)
+function [xHat, S] = omp(y, A, tol, rtol)
 %OMP Performs OMP given measurements vector y and sensing matrix A
 arguments
     y; A;
     tol = 1e-10;
+    rtol = 1e-3;
 end
 % Initialize algorithm
 xHat = zeros(size(A, 2), 1);
@@ -21,7 +22,11 @@ while any(abs(y - A * xHat) ./ abs(y) > tol)
 
     % Perform LS approximation
     As = A(:, S(1:i));
-    At = (As' * As) \ As';
+    Ast = As' * As;
+    if rcond(Ast) < rtol
+        break
+    end
+    At = Ast \ As';
     xHat = zeros(size(xHat));
     xHat(S(1:i)) = At * y;
 end
