b57f1523802c4be226dd4119bea880aff0e2e7b3
Rodrigo Fischer <rodrfischer@gmail.com>
2023-05-02 17:04:35 +0200

    New OMP Algorithm

--- omp_acc_A.m
+++ omp_acc_A.m
@@ -12,6 +12,10 @@ grid_height = 30;               % grid height (cm)
 num_antennas = 46;              % number of antennas in array (on each side)
 random_seed = 'shuffle';        % 'shuffle' for random; any int for reproducibility
 nIter = 1000;                   % number of attempts 
+sim_type = 'best';              % choose between 'best' to compute the best
+                                % and average performance, or 'random' to
+                                % make A random
+nSearchIter = 1000;             % how many iterations of random search for good A
 
 % Random generator
 rStr = RandStream('mcg16807', 'Seed', random_seed);
@@ -27,36 +31,94 @@ step = 2;
 start = 8;
 stop = 32;
 
-accuracy = zeros(N, 1);
+if strcmp(sim_type, 'best')
+    accuracy = zeros(N, 2);
+else
+    accuracy = zeros(N, 1);
+end
+
+% 'A' database directory
+if ~exist('A_database\', 'dir')
+    mkdir('A_database\')
+end
+
 for Neff = start:step:stop
 
-    right = 0;
+    right1 = 0;
+    right2 = 0;
     M = Neff ^ 2;
 
-    for i = 1:nIter
-        % Progress
-        fprintf('Iter: %d, M = %d\n', i, M);
+    if strcmp(sim_type, 'best')
+        fileNameA = fullfile('A_database', sprintf('A_%d', Neff));
+        if ~exist(fullfile('A_database', fileNameA), 'file')
+            [ACand, AMean] = random_search_A(rStr, AiF, numRX, num_antennas, Neff, nSearchIter, ...
+                fileNameA);
 
-        txPerm = randperm(rStr, 2 * num_antennas, Neff)';
-        rxPerm = randperm(rStr, 2 * num_antennas, Neff)';
-        ATrim = trim_A(AiF, txPerm, rxPerm, numRX);
+        else
+            loadA = load(fileNameA);
+            ACand = loadA.ACand;
+            AMean = loadA.AMean;
 
-        % Generate sparse x
-        [x, ~] = sparse_x(rStr, N, s, true);
-    
-        % Sample 
-        y = ATrim * x;
-    
-        % Perform OMP
-        [xHat, ~] = omp(y, ATrim);
-        error = sum(abs(xHat - x) .^ 2);
-    
-        if error < 1e-10
-            right = right + 1;
         end
-    end
+        
+        for i = 1:nIter
+            % Progress
+            fprintf('Iter: %d, M = %d\n', i, M);
+    
+            % Generate sparse x
+            [x, ~] = sparse_x(rStr, N, s, true);
+        
+            % Sample 
+            y1 = ACand * x;
+            y2 = AMean * x;
+        
+            % Perform OMP
+            [xHat1, ~] = omp(y1, ACand);
+            [xHat2, ~] = omp(y2, AMean);
+            error1 = sum(abs(xHat1 - x) .^ 2);
+            error2 = sum(abs(xHat2 - x) .^ 2);
+        
+            if error1 < 1e-10
+                right1 = right1 + 1;
+            end
 
-    accuracy(M) = right / nIter;
-    save(fileName, 'nIter', 'N', 's', 'accuracy', 'step', 'start', 'stop');
+            if error2 < 1e-10
+                right2 = right2 + 1;
+            end
+        end
+    
+        accuracy(M, 1) = right1 / nIter;
+        accuracy(M, 2) = right2 / nIter;
+        save(fileName, 'nIter', 'N', 's', 'accuracy', 'step', 'start', 'stop', 'Neff');
+
+    else
+
+        for i = 1:nIter
+            % Progress
+            fprintf('Iter: %d, M = %d\n', i, M);
+    
+            txPerm = randperm(rStr, 2 * num_antennas, Neff)';
+            rxPerm = randperm(rStr, 2 * num_antennas, Neff)';
+            ATrim = trim_A(AiF, txPerm, rxPerm, numRX);
+    
+            % Generate sparse x
+            [x, ~] = sparse_x(rStr, N, s, true);
+        
+            % Sample 
+            y = ATrim * x;
+        
+            % Perform OMP
+            [xHat, ~] = omp(y, ATrim);
+            error = sum(abs(xHat - x) .^ 2);
+        
+            if error < 1e-10
+                right1 = right1 + 1;
+            end
+        end
+    
+        accuracy(M) = right1 / nIter;
+        save(fileName, 'nIter', 'N', 's', 'accuracy', 'step', 'start', 'stop');
+
+    end
 
 end
