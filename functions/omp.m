ee4d2d79100fbb6a45816d564064368184550e64
Rodrigo Fischer <rodrfischer@gmail.com>
2023-05-31 19:24:44 +0200

    Replace pinv in OMP

--- functions/omp.m
+++ functions/omp.m
@@ -1,9 +1,8 @@
-function [xHat, S] = omp(y, A, tol, rtol)
+function [xHat, S] = omp(y, A, tol)
 %OMP Performs OMP given measurements vector y and sensing matrix A
 arguments
     y; A;
     tol = 1e-10;
-    rtol = 1e-10;
 end
 % Initialize algorithm
 xHat = zeros(size(A, 2), 1);
@@ -22,13 +21,9 @@ while any(abs(y - A * xHat) > tol)
 
     % Perform LS approximation
     As = A(:, S(1:i));
-    Ast = As' * As;
-    if rcond(Ast) < rtol
-        break
-    end
-    At = Ast \ As';
+    
     xHat = zeros(size(xHat));
-    xHat(S(1:i)) = At * y;
+    xHat(S(1:i)) = lsqminnorm(As, y);
 end
 end
 
