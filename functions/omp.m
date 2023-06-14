8738146bec2c1d544fca44fc7d1aa7623422f674
Rodrigo Fischer <rodrfischer@gmail.com>
2023-06-14 16:27:17 +0200

    Fail Safe OMP

--- functions/omp.m
+++ functions/omp.m
@@ -10,20 +10,24 @@ S = [];
 
 % Begin
 i = 0;
-while any(abs(y - A * xHat) > tol)
+while any(abs(y - A * xHat) > tol) && (i < size(A, 1))
     i = i + 1;
 
     % Get index of smallest error 
-    [~, idx] = sort(-abs(A' * (y - A * xHat)));
+    [~, idx] = max(abs(A' * (y - A * xHat)));
 
     % Expand the support
-    S = [S; idx(1)];
+    if ~ismember(idx, S)
+        S = [S; idx];
+    else
+        break
+    end
 
     % Perform LS approximation
     As = A(:, S(1:i));
     
     xHat = zeros(size(xHat));
-    xHat(S(1:i)) = lsqminnorm(As, y);
+    xHat(S(1:i)) = pinv(As) * y;
 end
 end
 
