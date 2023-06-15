cfb02a8557aa19af516c7c573875e74dc012e2ec
Rodrigo Fischer <rodrfischer@gmail.com>
2023-06-15 20:17:48 +0200

    New OMP Version

--- functions/omp.m
+++ functions/omp.m
@@ -10,7 +10,16 @@ S = [];
 
 % Begin
 i = 0;
-while any(abs(y - A * xHat) > tol) && (i < size(A, 1))
+
+% We use 'all' instead of 'any' because usually when OMP finds the right
+% solution, in the same iteration that all(abs(y - A * xHat) > tol) becomes
+% false so does the statement any(abs(y - A * xHat) > tol), that means as
+% soon as one entry is minimized so are the others
+
+% Doing this we can stop OMP at fewer iterations, since for the cases a
+% mistake happens it is not worth checking until any(...) is false
+
+while all(abs(y - A * xHat) > tol) && (i < size(A, 1))
     i = i + 1;
 
     % Get index of smallest error 
@@ -20,14 +29,20 @@ while any(abs(y - A * xHat) > tol) && (i < size(A, 1))
     if ~ismember(idx, S)
         S = [S; idx];
     else
+        % This case happens when all the elements of abs(A' * (y - A *
+        % xHat)) are almost zero, so it has essentially found the LS
+        % solution and 'tol' is too small to be met 
         break
     end
 
     % Perform LS approximation
     As = A(:, S(1:i));
-    
     xHat = zeros(size(xHat));
-    xHat(S(1:i)) = pinv(As) * y;
+    
+    Ast = As' * As;
+    At = Ast \ As';
+    xHat(S(1:i)) = At * y;
+
 end
 end
 
